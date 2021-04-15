606fde3c684c733bab11ec349967ee8ab43399d2
carlassaraf <fabrizio.carlassara@gmail.com>
2021-04-15 20:54:28 -0300

    Changed server IP

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import sys
 
 HEADER = 64
 PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
+SERVER = '0.0.0.0'
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = '!DISCONNECT'
